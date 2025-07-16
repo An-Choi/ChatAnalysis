bb12f2d86ab78b548107ca60dd2914284f329e82
louis <louisanseung@naver.com>
2025-07-16 19:07:32 +0900

    텍스트 파일 전처리 함수 추가

--- backend/chatbot/src/main/java/com/anchoi/chatbot/data_parsing/data_parsing.py
+++ backend/chatbot/src/main/java/com/anchoi/chatbot/data_parsing/data_parsing.py
@@ -1,14 +1,71 @@
 import os
 import csv
 import json
+import re
 
 UPLOAD_DIR = "uploads"
 
 ##텍스트 파일 전처리
-def processing_text_file(file_path, proccessed):
-    ##텍스트 !!!!! 
-    ##branch test
-    NotImplemented
+def processing_text_file(file_path, processed):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    
+
+    #학습하는 대상
+    first_line = lines[0]
+    train_name = first_line.split('님과 카카오톡 대화')[0].strip()  
+    
+    #메세지 패턴
+    msg_pattern = re.compile(r"\[(.*?)\] \[(오전|오후) (\d{1,2}):(\d{2})\] (.+)")
+
+    #날짜 패턴
+    date_pattern = re.compile(r"-+ (\d{4})년 (\d{1,2})월 (\d{1,2})일.*-+")
+    
+    curr_date = None
+    prev_sender = None
+
+    for line in lines: 
+        line = line.strip()
+
+        #날짜 탐지 및 처리
+        match_date = date_pattern.match(line)
+        if match_date:
+            year = match_date.group(1)
+            month = match_date.group(2)
+            date = match_date.group(3)
+            curr_date = f"{int(year):04d}-{int(month):02d}-{int(date):02d}"
+            continue
+
+
+        #메세지 탐지 및 처리
+        match_msg = msg_pattern.match(line)
+        if match_msg:
+            sender = match_msg.group(1)
+            am_pm= match_msg.group(2)
+            hour = match_msg.group(3)
+            sec = match_msg.group(4)
+            msg = match_msg.group(5)
+
+            #오전 오후 처리
+            hour = int(hour)
+            if am_pm == "오후" and hour!=12:
+                hour += 12
+            elif am_pm == "오전" and hour == 12:
+                hour = 0
+
+            time = f"{curr_date} {hour}:{sec}"
+            processed.append({
+                "sender":sender,
+                "message":msg,
+                "time":time,
+                "trainer": train_name == sender
+            })
+
+            prev_sender = sender
+    
+        
+
+
 ##csv파일 전처리
 def processing_csv_file(file_path, processed):
     NotImplemented
