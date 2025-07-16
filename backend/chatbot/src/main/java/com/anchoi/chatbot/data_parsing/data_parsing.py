add921bc9e2a8da1497cda076108faae64cea946
byungju0 <byungju8797@naver.com>
2025-07-16 21:10:13 +0900

    row 병합 추가

--- backend/chatbot/src/main/java/com/anchoi/chatbot/data_parsing/data_parsing.py
+++ backend/chatbot/src/main/java/com/anchoi/chatbot/data_parsing/data_parsing.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import json
+import pandas as pd
+import unicodedata
 
 UPLOAD_DIR = "uploads"
 
@@ -8,9 +10,57 @@ UPLOAD_DIR = "uploads"
 def processing_text_file(file_path, proccessed):
     NotImplemented
 ##csv파일 전처리
-##adsf
 def processing_csv_file(file_path, processed):
-    NotImplemented
+    ##파일 읽기
+    df = pd.read_csv(file_path)
+
+    ##파일 이름에서 학습 대상 이름 추출
+    ##한글 비교 시 유니코드 정규화 통일 후 비교
+    filename = os.path.splitext(os.path.basename(file_path))[0]
+    filename_nfc = unicodedata.normalize('NFC', filename)
+    users = df['User'].astype(str).str.strip().apply(lambda x: unicodedata.normalize('NFC', x))
+    target = None
+    for user in users:
+        if user in filename_nfc:
+            target = user
+            break
+    
+    ##한 사용자가 연속으로 메세지를 보낸 경우 하나의 row로 처리
+    ##비어있는 new_df 생성 후 처리된 row를 추가
+    new_df = pd.DataFrame(columns=['Date', 'User', 'Message'])
+    current_user = None
+    current_message = ""
+    current_date = ""
+
+    for index, row in df.iterrows():
+        if current_user is None:
+            current_date = row['Date']
+            current_user = row['User']
+            current_message += row['Message']
+        elif current_user != row['User']:
+            new_row = pd.DataFrame([{
+                'Date': current_date,
+                'User': current_user,
+                'Message': current_message
+            }])
+            new_df = pd.concat([new_df, new_row], ignore_index=True)
+            current_date = row['Date']
+            current_user = row['User']
+            current_message = row['Message']
+        else:
+            current_message += "\n" + row['Message']
+
+    if current_user is not None:
+        new_row = pd.DataFrame([{
+            'Date': current_date,
+            'User': current_user,
+            'Message': current_message
+        }])
+        new_df = pd.concat([new_df, new_row], ignore_index=True)
+
+    print(df.head(5))
+    print(target)
+    print(new_df.head(5))
 
 ##실행 함수
 def execute_files(filename):
@@ -40,5 +90,5 @@ if __name__ == "__main__":
     result = []
     for filename in os.listdir(UPLOAD_DIR):
         result = execute_files(filename)
-
+    
     ##json 파일 백엔드 서버로 보내기
