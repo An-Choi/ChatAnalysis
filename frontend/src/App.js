09d5e5da01096054ac225cacd0f2c5c9975610e1
byungju0 <byungju8797@naver.com>
2025-11-14 16:23:29 +0900

    loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Home from './pages/Home';
-import ChatBot from './components/ChatBot'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Home from "./pages/Home";
+import ChatBot from "./components/ChatBot";
+import Loading from "./components/Loading";
+import FileUpload from "./components/FileUpload";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -12,8 +14,9 @@ function App() {
         <Link to="/">업로드</Link> | <Link to="/chat">챗봇</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<FileUpload />} />
         <Route path="/chat" element={<ChatBot />} />
+        <Route path="/loading" element={<Loading />} />
       </Routes>
     </Router>
   );
